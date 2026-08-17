a35d9d56b2755e379eb259f118a3c3d57b066472
Minjie Wan （万敏杰） <38133231+MinjieWan@users.noreply.github.com>
2026-08-17 23:15:03 +0800

    Add files via upload

---
+++ STDF-Net.py
@@ -0,0 +1,3 @@
+Thank you for your interest in STDF-Net. Currently, only part of the implementation is publicly available, as the related manuscript is still under review. 
+The complete source code, including the full network implementation, training and evaluation scripts, will be released after the paper is accepted for publication.
+Thank you for your understanding and patience.
